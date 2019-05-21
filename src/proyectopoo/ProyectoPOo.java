91fdead6df6ffa383886bd4e8affddc85b9ea5de
cris1 <cris1@DESKTOP-1BIARH0>
2019-05-21 14:39:55 -0500

    hice esto

--- src/proyectopoo/ProyectoPOo.java
+++ src/proyectopoo/ProyectoPOo.java
@@ -17,7 +17,7 @@ public class ProyectoPOo {
     public static void main(String[] args) {
        iPets ip = new iPets();
        ip.setVisible(true);
-        
+        System.out.println("hola wapa");
     }
     
 }
